1cea19abe0fab2e22c06828f79bdb1237378e57e
CRGuarda <crguarda95@gmail.com>
2023-06-02 14:52:39 -0500

    discordjs added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-import { Client } from '@discordjs/core'
+import 'dotenv/config'
+import { Client, GatewayIntentBits } from 'discord.js'
+
+const TOKEN = process.env.DISCORD_TOKEN
+let count = 0
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+})
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('interactionCreate', (interaction) => {
+  if (!interaction.isChatInputCommand() || interaction.user.bot) return
+
+  if (interaction.commandName === 'add') {
+    const { user } = interaction.options.get('user')
+    if (user.bot) return interaction.reply(`Can't make a count of a bot`)
+
+    console.log(user.id)
+    count += 1
+
+    return interaction.reply(`Added the count for ${user.toString()}. Count: ${count}!`)
+  }
+})
+
+// client.on('messageCreate', (msg) => {
+//   if (msg.author.bot) return
+
+//   if (msg.content.startsWith('help')) {
+//     const user = msg.mentions.users.first()
+//     return msg.channel.send(`hola ${user.toString()}`)
+//   }
+// })
+
+client.login(TOKEN)
---
+++ src/rest-commands.js
@@ -0,0 +1,32 @@
+import 'dotenv/config'
+import { REST, Routes, ApplicationCommandOptionType } from 'discord.js'
+
+const TOKEN = process.env.DISCORD_TOKEN
+const CLIENT_ID = process.env.CLIENT_ID
+
+const commands = [
+  {
+    name: 'add',
+    description: 'Increment the count',
+    options: [
+      {
+        name: 'user',
+        description: 'Pick a user to increment the count',
+        type: ApplicationCommandOptionType.Mentionable,
+        required: true,
+      },
+    ],
+  },
+]
+
+const rest = new REST({ version: '10' }).setToken(TOKEN)
+
+try {
+  console.log('Started refreshing application (/) commands.')
+
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands })
+
+  console.log('Successfully reloaded application (/) commands.')
+} catch (error) {
+  console.error(error)
+}
